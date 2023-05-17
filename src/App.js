a3455aa8be93872989a873c352b0063990424d30
user941211 <common941211@gmail.com>
2023-05-18 03:11:08 +0900

    코인 시세 api 가져오기 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 //import { ReactDOM } from 'react';
 
 function App() {
+  /*todo_list 출력
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
@@ -18,6 +19,7 @@ function App() {
   console.log(toDos);
   //form 내에서 button이 한개만 있으면 자동적으로 submit의 역할을 한다.
   return (
+    
     <div>
       <h1>ToDo 리스트!!({toDos.length})</h1>
       <form onSubmit={onSubmit}>
@@ -35,6 +37,42 @@ function App() {
           <li key={index}>{item}</li>
         ))}
       </ul>
+    </div>
+    
+  );
+  */
+  //let dollar;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  //위에 줄은 비어있는 배열로 지정해서 undefined 되지 않게 한다.
+  useEffect(() =>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, []);
+  return(
+    <div>
+      <h1>코인판!!{loading ? "" : `(${coins.length})`}</h1>
+      <select>
+        {coins.map((coin) =>(
+          <option>
+            {coin.symbol};
+          </option>
+        ))}
+      </select>
+      {loading ? <strong>Loading...</strong>:(
+        <select>
+        {coins.map((coin,) =>(
+          <option>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} 달러 
+          </option>
+        ))}
+      </select>
+      )}
+      
     </div>
   );
 }
