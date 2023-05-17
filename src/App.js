72e14b1717163e41214d9554063ce705793f55a1
user941211 <common941211@gmail.com>
2023-05-17 20:25:50 +0900

    배열에 값 넣고 실시간 변화 보기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
 import {useState} from "react";
-
+//import { ReactDOM } from 'react';
 
 function App() {
-  const[toDo, setToDo] = useState("");
+  const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
   //console.log(toDo);
   const onSubmit = (event) => {
     event.preventDefault();
-    if(toDo ===""){return;}
-    
-    setToDos(currentArray => [toDo, ...currentArray]);
+    if(toDo ===""){
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
-  }
+  };
   console.log(toDos);
   //form 내에서 button이 한개만 있으면 자동적으로 submit의 역할을 한다.
   return (
     <div>
-      <h1>ToDo 리스트!!</h1>
+      <h1>ToDo 리스트!!({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input 
           onChange={onChange}
@@ -27,8 +28,13 @@ function App() {
           placeholder="입력"
         />
         <button>ADD</button>
-        
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item,index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
